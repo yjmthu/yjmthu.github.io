9ff7cf4411524ccc5f99c4be8379f8ca2e0f85f9
Wiley2046 <yjmthu@outlook.com>
2023-01-27 21:30:43 +0800

    词汇联想加强，页面适应加强

--- vua/main.js
+++ vua/main.js
@@ -1,47 +1,76 @@
-const suggestDOM = document.getElementById("search-suggest");
-const suggestBoxDOM = document.getElementById("suggest-box")
+const suggestDOM = document.getElementById('search-suggest');
+const suggestBoxDOM = document.getElementById('suggest-box')
 const inputDOM = document.getElementById('search-input')
 
 var script = null
 
-function suggest_callback(jsObj) {
-  var d = jsObj.AS.Results[0].Suggests;
-  var html = '';
-  for(var i = 0; i!=d.length; i++){
-    html += "<li>" + d[i].Txt + "</li>"
-  }
-  suggestDOM.innerHTML = html;
-  suggestBoxDOM.style.display = "block";
+function get_result(keyword) {
+  window.location.href = 'https://cn.bing.com/search?q=' + keyword
 }
 
-function submit_search(event) {
+function suggest_callback(jsObj) {
+  if (!jsObj) return
+  let res = jsObj.AS.Results
+  let html = '';
+  if (res) {
+    let data = res[0].Suggests;
+    for (var i = 0; i != data.length; i++){
+      html += '<li>' + data[i].Txt + '</li>'
+    }
+  } else {
+    html = '<i>没有匹配结果<i>'
+  }
+  suggestDOM.innerHTML = html;
+  suggestBoxDOM.style.display = 'block';
+}
+
+
+inputDOM.onkeydown = function submit_search(event) {
   let e = window.event || event
   const keyword = inputDOM.value
   if (e.keyCode == '\r'.charCodeAt()) {
     if (!keyword.length) {
-      suggestBoxDOM.style.display = "none";
+      suggestBoxDOM.style.display = 'none';
       return
     }
-    window.location.href = 'https://cn.bing.com/search?q=' + keyword
-  } else if (e.keyCode == 8) {
-    if (keyword.length == 1) {
-      suggestBoxDOM.style.display = "none";
-      return
-    }
+    get_result(keyword)
   }
-  // ajax_get("http://api.bing.com/qsonhs.aspx?q=" + keyword, suggest_callback);
+}
+
+inputDOM.oninput = function get_suggets() {
+  if (!inputDOM.value.length) {
+    suggestBoxDOM.style.display = 'none';
+    return
+  }
+  // ajax_get('http://api.bing.com/qsonhs.aspx?q=' + keyword, suggest_callback);
   if (script) {
     document.body.removeChild(script)
   }
-  script = document.createElement("script")
-  script.src = "http://api.bing.com/qsonhs.aspx?type=cb&q=" + keyword + "&cb=suggest_callback"
+  script = document.createElement('script')
+  script.src = 'http://api.bing.com/qsonhs.aspx?type=cb&q=' + inputDOM.value + '&cb=suggest_callback'
   document.body.appendChild(script)
 }
 
+inputDOM.onfocus = () => {
+  if (inputDOM.value.length) {
+    suggestBoxDOM.style.display = 'block';
+  }
+}
+
+inputDOM.onblur = () => {
+  suggestBoxDOM.style.display = 'none';
+}
+
 suggestBoxDOM.onclick = e => {
-  e = e || window.event;//这一行及下一行是为兼容IE8及以下版本
-  var target = e.target || e.srcElement;
-  if (target.tagName.toLowerCase() === "li") {
-    window.location.href = 'https://cn.bing.com/search?q=' + target.innerHTML
+  e = e || window.event
+  var target = e.target || e.srcElement
+  if (target.tagName.toLowerCase() === 'li') {
+    get_result(target.innerHTML)
+  }
+}
+
+document.getElementById('icon-search').onclick = e => {
+  if (inputDOM.value.length) {
+    get_result(inputDOM.value)
   }
 }
